09e6bde7cf0aba8179e3aeeb31aaefad71df1539
Metehan <metehan.bolat.ie@gmail.com>
2022-11-15 16:14:34 +0300

    PersonUIItem was completed.

---
+++ app/src/main/java/com/metehanbolat/draganddropcompose/PersonUIItem.kt
@@ -0,0 +1,9 @@
+package com.metehanbolat.draganddropcompose
+
+import androidx.compose.ui.graphics.Color
+
+data class PersonUIItem(
+    val name: String,
+    val id: String,
+    val backgroundColor: Color
+)
